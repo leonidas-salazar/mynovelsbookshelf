660ae76d4201e4db0d369eb71506579f298298be
Leônidas Alves Salazar <salazar.leonidas@gmail.com>
2025-09-07 22:35:58 -0300

    feat: initial commit

---
+++ MyNovelsBookshelf/src/Backend/MyNovelsBookshelf.Api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNovelsBookshelf.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyNovelsBookshelf/src/Backend/MyNovelsBookshelf.Domain/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace MyNovelsBookshelf.Domain.Entities
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedOn { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}

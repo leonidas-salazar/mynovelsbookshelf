bc9c6e35def849b85248d1726063a3e923da7a22
Leônidas Alves Salazar <salazar.leonidas@gmail.com>
2025-09-08 00:21:20 -0300

    feat: adding custom messages and custom exceptions

---
+++ MyNovelsBookshelf/src/Shared/MyNovelsBookshelf.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,13 @@
+﻿namespace MyNovelsBookshelf.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Errors { get; set; }
+
+        public ResponseErrorJson(string error)
+        {
+            Errors = [error];
+        }
+        public ResponseErrorJson(IList<string> errors) => Errors = errors;
+    }
+}
---
+++ MyNovelsBookshelf/src/Shared/MyNovelsBookshelf.Communication/Responses/Users/ResponseRegisterUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyNovelsBookshelf.Communication.Responses.Users
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Username { get; set; } = string.Empty;
+    }
+}
